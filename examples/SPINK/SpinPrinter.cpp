305f610250dff696bc987da7f337d90802132cfb
nmalitsky <nmalitsky@9fb5c71f-954c-0410-a71a-f752f27b4993>
2009-08-13 19:46:46 +0000

    updated DipoleErTracker

--- examples/SPINK/SpinPrinter.cpp
+++ examples/SPINK/SpinPrinter.cpp
@@ -37,13 +37,15 @@ void SpinPrinter::write(int iturn, int ip, PAC::Bunch& bunch)
 
       double wp_time = t0 + (-ct /UAL::clight );
 
+      double s = sqrt(sx*sx + sy*sy + sz*sz);
+
       double spin_g2 = (sx*px+sy*py+sz*(1.0+x/100.0))/sqrt(sx*sx+sy*sy+sz*sz)/sqrt(px*px+py*py+(1.0+x/100.0)*(1.0+x/100.0));
 
       char endLine = '\0';
       char line2[200];
 
-      sprintf(line2, "%1d %7d    %-15.9e %-16.7e %-16.7e %-16.7e %-16.7e %c",
-	      ip, iturn, wp_time, sx, sy, sz, spin_g2, endLine);
+      sprintf(line2, "%1d %7d    %-15.9e %-16.7e %-16.7e %-16.7e %-16.7e %-16.7e %c",
+	      ip, iturn, wp_time, sx, sy, sz, s, spin_g2, endLine);
 
       output << line2 << std::endl;
 }
