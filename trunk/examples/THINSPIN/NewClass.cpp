97141a7fb2f9818b5558d0535454ba6458b6976b
Richard.Talman <Richard.Talman@9fb5c71f-954c-0410-a71a-f752f27b4993>
2010-01-06 15:59:49 +0000

    adding NewClass

---
+++ trunk/examples/THINSPIN/NewClass.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   NewClass.cpp
+ * Author: ualusr2010
+ * 
+ * Created on January 6, 2010, 9:55 AM
+ */
+
+#include "NewClass.h"
+
+NewClass::NewClass() {
+}
+
+NewClass::NewClass(const NewClass& orig) {
+}
+
+NewClass::~NewClass() {
+}
+
