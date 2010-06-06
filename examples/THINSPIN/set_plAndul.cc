39f8023c578fabc6f934c6366e733c0808b7cf0e
richard.talman <richard.talman@9fb5c71f-954c-0410-a71a-f752f27b4993>
2010-06-06 15:56:26 +0000

    Added set_plAndul.cc, globalBlock.cc, PropagatorSequence.cc, commonAlgorithm.iccand modified rfCavity.hh

---
+++ examples/THINSPIN/set_plAndul.cc
@@ -0,0 +1,19 @@
+  pl.set0( e0 + p0 * p.getDE());
+  pl.set1(-p0 * p.getPX());
+  pl.set2(-p0 * p.getPY());
+  pl.set3(-sqrt(pl.get0()*pl.get0() - m0*m0 - pl.get1()*pl.get1() - pl.get2()*pl.get2()));
+
+//std::cout << "Lab:  pl.get0() " << pl.get0() <<  " pl.get1()  " << pl.get1()  << " pl.get2()  " <<  pl.get2() << "  pl.get3() " <<  pl.get3() << "\n";
+//THINSPIN::fourVector er;
+//lorentzTransform(pl, betal, gamma, er);
+//std::cout << "Rest: er.get0() " << er.get0() <<  " er.get1()  " << er.get1()  << " er.get2()  " <<  er.get2() << "  er.get3() " << er.get3() << "\n";
+//THINSPIN::fourVector plM;
+//lorentzTransform(er, betal*=(-1), gamma, plM);
+//std::cout << "Lab: plM.get0() " << plM.get0() << " plM.get1() " << plM.get1() << " plM.get2() " << plM.get2() << "  plM.get3() " <<  plM.get3() << "\n\n"; 
+
+  ul.set0(pl.get0()/m0);
+  ul.set1(pl.get1()/m0);            //  negativeness due to covariant already included above
+  ul.set2(pl.get2()/m0);
+  ul.set3(pl.get3()/m0);
+
+  gl = pl.get0()/m0;
