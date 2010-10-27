9961ef8d1e44d3b65cf95f603e9b587d251a6483
JohnDTalman <JohnDTalman@9fb5c71f-954c-0410-a71a-f752f27b4993>
2010-10-27 19:01:46 +0000

    started ETEAPOT

---
+++ trunk/codes/ETEAPOT/src/ETEAPOT/Base/Def.hh
@@ -0,0 +1,20 @@
+// Library       : TEAPOT
+// File          : TEAPOT/Base/Def.hh
+// Copyright     : see Copyright file
+// Author        : L.Schachinger and R.Talman
+// C++ version   : N.Malitsky 
+
+#ifndef TEAPOT_DEF_HH
+#define TEAPOT_DEF_HH
+
+/**
+   Thin Element Accelerator Program for Optics and Tracking 
+   (collection of symplectic trackers and mappers, collection 
+   of correction algorithms), 
+   authors: Richard Talman and Lindsay Schachinger
+   <p> (under construction).
+ */
+namespace ETEAPOT {
+}
+
+#endif
