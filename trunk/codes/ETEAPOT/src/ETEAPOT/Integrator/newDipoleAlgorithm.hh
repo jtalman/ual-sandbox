077d89caf39ccd01bf123f2dc817387804cc8c69
JohnDTalman <JohnDTalman@9fb5c71f-954c-0410-a71a-f752f27b4993>
2011-01-10 18:33:16 +0000

    design parameters

---
+++ trunk/codes/ETEAPOT/src/ETEAPOT/Integrator/newDipoleAlgorithm.hh
@@ -0,0 +1,62 @@
+// Library       : ETEAPOT
+// File          : ETEAPOT/Integrator/newDipoleAlgorithm.hh
+// Copyright     : see Copyright file
+// Author        : L.Schachinger and R.Talman
+// C++ version   : N.Malitsky 
+
+#ifndef UAL_ETEAPOT_NEW_DIPOLE_ALGORITHM_HH
+#define UAL_ETEAPOT_NEW_DIPOLE_ALGORITHM_HH
+
+//#include <stdio.h>
+//#include <stdlib.h>
+  #include <cstdlib>
+//#include <math.h>
+  #include <cmath>
+
+#include "ETEAPOT/Integrator/ElectricAlgorithm.hh"
+
+namespace ETEAPOT {
+ 
+  /** A template of the dipole algorithm used by the conventional tracker and DA integrator */
+
+  template<class Coordinate, class Coordinates> class newDipoleAlgorithm 
+    : public ElectricAlgorithm<Coordinate, Coordinates> {
+
+  public:
+
+    /** Constructor */
+    newDipoleAlgorithm();
+
+    /** Destructor */
+    ~newDipoleAlgorithm();
+
+    /** Propagates a probe coordinates through the bend*/
+    void passBend(const DipoleData& ddata, const ElectricData& mdata, Coordinates& p, Coordinates& tmp, double v0byc, double e0, double m0, double q0,double L0);
+
+    /** Propagates a probe coordinates through the bend slice*/
+    void passBendSlice(const ElemSlice& slice, Coordinates& p, Coordinates& tmp, double v0byc);
+
+    /** Applies a thin bend kick*/
+    void applyThinBendKick(const DipoleData& data, 
+                           const ElectricData& mdata, double rkicks,
+                           Coordinates& p, double v0byc);
+
+    /** Calculates the delta path*/
+    void deltaPath(const ElemSlice& slice, Coordinates& p, Coordinates& tmp, double v0byc);
+
+    void enterBendCorrection(double q0,double E0,double R0,double r);
+    void traverseSplitBendExactly(const ElemSlice& slice, Coordinates& p, Coordinates& tmp, double v0byc, double e0, double p0, double m0);
+    void leaveBendCorrection();
+    void handleSplitBendBoundary();
+
+    double getPotentialEnergy(double q0,double E0,double R0,double r){
+       return q0*E0*R0*log(r/R0); 
+    }
+
+  };
+
+}
+
+#include "ETEAPOT/Integrator/newDipoleAlgorithm.icc"
+
+#endif
