012086746fcd04bc7baa819da42b96b9f996d6cb
JohnDTalman <JohnDTalman@9fb5c71f-954c-0410-a71a-f752f27b4993>
2011-01-11 21:25:01 +0000

    design parameters

--- trunk/examples/ETEAPOT/extractParameters.h
+++ trunk/examples/ETEAPOT/extractParameters.h
@@ -1,21 +1,23 @@
- std::string sxfFile = "./data/";
- sxfFile += argv[2];
- sxfFile += ".sxf";
+   std::string sxfFile = argv[1];
+// std::string sxfFile = "./data/";
+// sxfFile += argv[2];
+// sxfFile += ".sxf";
+
  std::string outputFile = "./out/cpp/";
- outputFile += argv[2];
+ outputFile += mysxfbase;
  outputFile += ".sxf";
  std::string mapFile = "./out/cpp/";
- mapFile += argv[2];
+ mapFile += mysxfbase;
  mapFile += ".map1";
  std::string twissFile = "./out/cpp/";
- twissFile += argv[2];
+ twissFile += mysxfbase;
  twissFile += ".twiss";
- std::string apdfFile = argv[1];
- double probe__dx0 = atof(argv[6]);
- double probe_dpx0 = atof(argv[7]);
- double probe__dy0 = atof(argv[8]);
- double probe_dpy0 = atof(argv[9]);
- double probe_cdt0 = atof(argv[10]);
+ std::string apdfFile = "./data/eteapot.apdf";
+ double probe__dx0 = .1;
+ double probe_dpx0 = 1e-3;
+ double probe__dy0 = .1;
+ double probe_dpy0 = 1e-4;
+ double probe_cdt0 = 1e-3;
 
  double probe__dz0 = probe_cdt0;
  double probe_dpz0 = 0;
@@ -31,8 +33,8 @@
  double probe_r0   = sqrt(probe_X0*probe_X0+probe_Y0*probe_Y0+probe_Z0*probe_Z0);
  double probeP0    = sqrt(probePX0*probePX0+probePY0*probePY0+probePZ0*probePZ0);
  std::string orbitFile = "./out/cpp/";
- orbitFile += argv[2];
+ orbitFile += mysxfbase;
  orbitFile += ".orbit";
 
- int split = atoi(argv[11]);
+ int split = 0;
  double probeEscr0 = sqrt(probeP0*probeP0+m0*m0)+q0*R0*log(probe_r0/R0) - sqrt(p0*p0+m0*m0);
