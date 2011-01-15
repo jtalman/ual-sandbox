88c65a9b80a1c2fc77921431a718d437a468dc38
JohnDTalman <JohnDTalman@9fb5c71f-954c-0410-a71a-f752f27b4993>
2011-01-15 20:11:38 +0000

    stubs latest

---
+++ trunk/codes/ETEAPOT/src/ETEAPOT/Integrator/print_enterBendCorrection.h
@@ -0,0 +1,16 @@
+std::cout << "p0      " << p0      << " (design momentum)\n";
+std::cout << "g0      " << g0      << " (design gamma)\n";
+std::cout << "v0      " << v0      << " (design velocity)\n";
+
+std::cout << "q       " << q       << " (charge numerical value from UAL::elemCharge)\n";
+std::cout << "GeVperJ " << GeVperJ << " (conversion)\n";
+
+std::cout << "e       " << e       << " (conserved total energy)\n";
+std::cout << "p[0]    " << p[0]    << " (probe x)\n";
+std::cout << "p[5]    " << p[5]    << " (probe Outside delta (Electrostatic_I Eqn. 1))\n";
+std::cout << "r       " << r       << " (probe radius neglecting y)\n";
+std::cout << "ePI     " << ePI     << " (probe potential energy)\n";
+std::cout << "eMI     " << eMI     << " (probe mechanical energy)\n";
+std::cout << "gO      " << gO      << " (probe gamma Outside)\n";
+std::cout << "gI      " << gI      << " (probe gamma Inside)\n";
+std::cout << "vI      " << vI      << " (probe velocity Inside)\n";
