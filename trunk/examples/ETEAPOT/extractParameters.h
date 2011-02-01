c67a06c03bcca638f3d968acd3b9d084f9476546
JohnDTalman@gmail.com <JohnDTalman@gmail.com@9fb5c71f-954c-0410-a71a-f752f27b4993>
2011-02-01 21:45:54 +0000

    4 bunch - cosine like x                     sine like x                 cosine like y                     sine like y

--- trunk/examples/ETEAPOT/extractParameters.h
+++ trunk/examples/ETEAPOT/extractParameters.h
@@ -13,28 +13,8 @@
  twissFile += mysxfbase;
  twissFile += ".twiss";
  std::string apdfFile = "./data/eteapot.apdf";
- double probe__dx0 = .1;
- double probe_dpx0 = 1e-3;
- double probe__dy0 = .1;
- double probe_dpy0 = 1e-4;
- double probe_cdt0 = 1e-3;
-
- double probe__dz0 = probe_cdt0;
- double probe_dpz0 = 0;
-
- double probe_X0   = R0 + probe__dx0;
- double probe_Y0   = probe__dy0;
- double probe_Z0   = probe__dz0;
-
- double probePX0   = probe_dpx0;
- double probePY0   = probe_dpy0;
- double probePZ0   = p0 + probe_dpz0;
-
- double probe_r0   = sqrt(probe_X0*probe_X0+probe_Y0*probe_Y0+probe_Z0*probe_Z0);
- double probeP0    = sqrt(probePX0*probePX0+probePY0*probePY0+probePZ0*probePZ0);
  std::string orbitFile = "./out/cpp/";
  orbitFile += mysxfbase;
  orbitFile += ".orbit";
 
  int split = 0;
- double probeEscr0 = sqrt(probeP0*probeP0+m0*m0)+q0*R0*log(probe_r0/R0) - sqrt(p0*p0+m0*m0);
