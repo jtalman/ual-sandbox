16e29798555f2872c65471647c40d7eac7de37e2
nmalitsky <nmalitsky@9fb5c71f-954c-0410-a71a-f752f27b4993>
2011-02-07 13:54:33 +0000

    added m_m (electric field parameter) into DipoleData

--- trunk/codes/ETEAPOT/src/ETEAPOT/Integrator/DipoleTracker.cc
+++ trunk/codes/ETEAPOT/src/ETEAPOT/Integrator/DipoleTracker.cc
@@ -23,7 +23,7 @@ ETEAPOT::DipoleTracker::DipoleTracker(const ETEAPOT::DipoleTracker& dt)
   : ETEAPOT::BasicTracker(dt)
 {
   m_data = dt.m_data;
-  m_edata = dt.m_edata;
+  m_mdata = dt.m_mdata;
 }
 
 ETEAPOT::DipoleTracker::~DipoleTracker()
@@ -48,19 +48,20 @@ void ETEAPOT::DipoleTracker::setLatticeElements(const UAL::AcceleratorNode& sequ
 void ETEAPOT::DipoleTracker::setLatticeElement(const PacLattElement& e)
 {
   m_data.setLatticeElement(e);
-  m_edata.setLatticeElement(e);
+  m_mdata.setLatticeElement(e);
 }
 
 void ETEAPOT::DipoleTracker::propagate(UAL::Probe& probe)
 {
-std::cout << "File " << __FILE__ << " line " << __LINE__ << " enter method void ETEAPOT::DipoleTracker::propagate(UAL::Probe& probe)\n";
-  UAL::AcceleratorNode fAN = getFrontAcceleratorNode();
-
   PAC::Bunch& bunch = static_cast<PAC::Bunch&>(probe);
-  
+
   PAC::BeamAttributes& ba = bunch.getBeamAttributes();
-  const PAC::BeamAttributes cba = ba;
+  double oldT = ba.getElapsedTime();
+  double e0 = ba.getEnergy(), m0 = ba.getMass();
+  double p0 = sqrt(e0*e0 - m0*m0);
   double v0byc = p0/e0;
+
+  double r0 = m_data.m_l/m_data.m_angle;
 
   PAC::Position tmp;
 
@@ -68,15 +69,14 @@ std::cout << "File " << __FILE__ << " line " << __LINE__ << " enter method void 
     if(bunch[ip].isLost()) continue;
     PAC::Position& p = bunch[ip].getPosition();
     tmp = p;
-#include "verboseBlock.h"
-    s_algorithm.passEntry(m_edata, p);
+    s_algorithm.passEntry(m_mdata, p);
+
+    s_algorithm.updateEnergy(r0, m_data.m_m, p, v0byc);
     s_algorithm.makeVelocity(p, tmp, v0byc);
     s_algorithm.makeRV(p, tmp, e0, p0, m0);
-std::cout << "File " << __FILE__ << " line " << __LINE__ << " about to  s_algorithm.passBend(m_data, m_edata, p, tmp, v0byc);\n";
-    s_algorithm.passBend(m_data, m_edata, p, tmp, v0byc, cba);
-//                      [      original interface      ] [central orbit ]
-std::cout << "File " << __FILE__ << " line " << __LINE__ << " back from s_algorithm.passBend(m_data, m_edata, p, tmp, v0byc, e0, p0, m0);\n";
-    s_algorithm.passExit(m_edata, p);
+
+    s_algorithm.passBend(m_data, m_mdata, p, tmp, v0byc);
+    s_algorithm.passExit(m_mdata, p);
     // testAperture(p);
   }
 
@@ -84,9 +84,9 @@ std::cout << "File " << __FILE__ << " line " << __LINE__ << " back from s_algori
   std::cout << "after dipole " << m_name << std::endl;
   for(int i =0; i < bunch.size(); i++){
     PAC::Position p = bunch[i].getPosition();
-    std::cout << i << " " 
-	      << p[0] << " " << p[1] << " " 
-	      << p[2] << " " << p[3] << " " 
+    std::cout << i << " "
+	      << p[0] << " " << p[1] << " "
+	      << p[2] << " " << p[3] << " "
 	      << p[4] << " " << p[5] << std::endl;
   }
   */
@@ -96,13 +96,4 @@ std::cout << "File " << __FILE__ << " line " << __LINE__ << " back from s_algori
   // Should be edited with the correct length for sbend and rbends
   ba.setElapsedTime(oldT + m_data.m_l/v0byc/UAL::clight);
 }
-
-
-
-
-
-
-
-
-
 
