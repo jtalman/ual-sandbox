a4ba3955800e91e105b58233565ef4b20e1c628d
nmalitsky <nmalitsky@9fb5c71f-954c-0410-a71a-f752f27b4993>
2011-02-07 14:21:25 +0000

    added addPotential

--- trunk/codes/ETEAPOT/src/ETEAPOT/Integrator/DipoleAlgorithm.hh
+++ trunk/codes/ETEAPOT/src/ETEAPOT/Integrator/DipoleAlgorithm.hh
@@ -1,20 +1,19 @@
 // Library       : ETEAPOT
 // File          : ETEAPOT/Integrator/DipoleAlgorithm.hh
 // Copyright     : see Copyright file
-// Author        : L.Schachinger and R.Talman
-// C++ version   : N.Malitsky 
 
-#ifndef UAL_ETEAPOT_DIPOLE_ALGORITHM_HH
-#define UAL_ETEAPOT_DIPOLE_ALGORITHM_HH
 
-#include "ETEAPOT/Integrator/ElectricAlgorithm.hh"
+#ifndef ETEAPOT_DIPOLE_ALGORITHM_HH
+#define ETEAPOT_DIPOLE_ALGORITHM_HH
+
+#include "ETEAPOT/Integrator/MltAlgorithm.hh"
 
 namespace ETEAPOT {
- 
+
   /** A template of the dipole algorithm used by the conventional tracker and DA integrator */
 
-  template<class Coordinate, class Coordinates> class DipoleAlgorithm 
-    : public ElectricAlgorithm<Coordinate, Coordinates> {
+  template<class Coordinate, class Coordinates> class DipoleAlgorithm
+    : public MltAlgorithm<Coordinate, Coordinates> {
 
   public:
 
@@ -25,19 +24,30 @@ namespace ETEAPOT {
     ~DipoleAlgorithm();
 
     /** Propagates a probe coordinates through the bend*/
-    void passBend(const DipoleData& ddata, const ElectricData& mdata, Coordinates& p, Coordinates& tmp, double v0byc);
+    void passBend(const DipoleData& ddata, const MltData& mdata,
+                  Coordinates& p, Coordinates& tmp,
+                  const PAC::BeamAttributes& ba);
 
     /** Propagates a probe coordinates through the bend slice*/
-    void passBendSlice(const ElemSlice& slice, Coordinates& p, Coordinates& tmp, double v0byc);
+    void passBendSlice(const ElemSlice& slice, 
+                       Coordinates& p, Coordinates& tmp,
+                       double v0byc);
 
     /** Applies a thin bend kick*/
-    void applyThinBendKick(const DipoleData& data, 
-                           const ElectricData& mdata, double rkicks,
-                           Coordinates& p, double v0byc);
+    void applyThinBendKick(const DipoleData& data,
+                           const MltData& mdata, double rkicks,
+                           Coordinates& p,
+                           double v0byc);
 
     /** Calculates the delta path*/
-    void deltaPath(const ElemSlice& slice, Coordinates& p, Coordinates& tmp, double v0byc);
+    void deltaPath(const ElemSlice& slice, 
+                   Coordinates& p, Coordinates& tmp,
+                   double v0byc);
 
+  public:
+
+      void addPotential(double r0, double m, Coordinates& p, double v0byc);
+      void removePotential(double r0, double m, Coordinates& p, double v0byc);
   };
 
 }
