be0fb5615276ded52d9d1cf9dd19ef3a7e66557e
JohnDTalman@gmail.com <JohnDTalman@gmail.com@9fb5c71f-954c-0410-a71a-f752f27b4993>
2011-02-12 16:42:42 +0000

    calculate L0 and E0 per element

--- trunk/examples/Cylindrical/getDesignBeam.h
+++ trunk/examples/Cylindrical/getDesignBeam.h
@@ -5,6 +5,7 @@ double t0=cba.getElapsedTime();
 double f0=cba.getRevfreq();
 double M0=cba.getMacrosize();
 double G0=cba.getG();
-double L0=cba.getL();
-double E0=cba.getE();
+
+//double L0=cba.getL();
+//double E0=cba.getE();
 //double R0=cba.getR();
