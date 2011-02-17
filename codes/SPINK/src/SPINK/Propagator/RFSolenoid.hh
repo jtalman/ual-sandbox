d305fa94da6a948329abaab1750232fd95c2f09b
JohnDTalman <JohnDTalman@gmail.com>
2011-02-17 19:44:49 +0000

    algorithm SPINK::SnakeTransform, sxf, apdf

---
+++ codes/SPINK/src/SPINK/Propagator/RFSolenoid.hh
@@ -0,0 +1,96 @@
+
+#ifndef UAL_SPINK_RFSOLENOID_HH
+#define UAL_SPINK_RFSOLENOID_HH
+
+#include "PAC/Beam/Position.hh"
+#include "SMF/PacLattElement.h"
+#include "SMF/PacElemRfCavity.h"
+#include "TEAPOT/Integrator/BasicTracker.hh"
+
+#include "SPINK/Propagator/SpinPropagator.hh" //AUL:27APR10
+
+namespace SPINK {
+
+  /** Spink-specific RF Solenoid (Tracker) */
+
+  class RFSolenoid : public TEAPOT::BasicTracker {
+
+  public:
+
+    /** Constructor */
+    RFSolenoid();
+
+    /** Copy Constructor */
+    RFSolenoid(const RFSolenoid& rft);
+
+    /** Destructor */
+    virtual ~RFSolenoid();
+
+    /** Returns a deep copy of this object (inherited from UAL::PropagatorNode) */
+    UAL::PropagatorNode* clone();
+
+    /** Set lattice elements (inherited from UAL::PropagatorNode */
+    void setLatticeElements(const UAL::AcceleratorNode& lattice, int i0, int i1, 
+			    const UAL::AttributeSet& beamAttributes);
+
+    //virtual void setLatticeElements(const UAL::AcceleratorNode& sequence, int i0, int i1, 
+    //				    const UAL::AttributeSet& attSet); //AUL:27APR10
+
+    /** Propagates a bunch of particles */
+    void propagate(UAL::Probe& probe);
+
+    /** Sets Rf parameters */
+    static void setRF(double V, double harmon, double lag) { 
+      m_V = V; m_h = harmon; m_lag = lag;
+    }
+
+    /** Pass ring length AUL:17MAR10 */
+    static void setCircum(double circum){circ = circum;}
+    static double circ ;
+
+     /** Setup a dump flag for diagnostics AUL:27APR10 */
+    static void setOutputDump(bool outdmp){coutdmp = outdmp;}
+    static bool coutdmp; 
+
+     /** Pass information on turn number for diagnostics AUL:27APR10 */
+    static void setNturns(int iturn){nturn = iturn;}
+    static int nturn ;
+  protected:
+
+    /** Sets the lattice element */
+    void setLatticeElement(const PacLattElement& e);
+
+    /** Propagates the particle through a drift */
+    void passDrift(double l, PAC::Position& p, double v0byc, double vbyc);
+
+  protected:
+
+    /** Element length */
+    double m_l;
+
+    /** Peak RF voltage [GeV] */ 
+    static double m_V;
+
+    /** Phase lag in multiples of 2 pi */
+    static double m_lag;
+
+    /** Harmonic number */
+    static double m_h;
+
+  private:
+
+    void init();
+    void copy(const RFSolenoid& rft);
+    
+  };
+
+  class RFSolenoidRegister
+  {
+    public:
+
+      RFSolenoidRegister();
+  };
+
+}
+
+#endif
