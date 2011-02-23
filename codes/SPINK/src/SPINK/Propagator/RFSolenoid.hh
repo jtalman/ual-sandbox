776406e48e08e63a420e0ade3b07c972e31ab85d
JohnDTalman <JohnDTalman@gmail.com>
2011-02-23 21:01:51 +0000

    cloned from SnakeTransform

--- codes/SPINK/src/SPINK/Propagator/RFSolenoid.hh
+++ codes/SPINK/src/SPINK/Propagator/RFSolenoid.hh
@@ -1,95 +1,120 @@
+#ifndef UAL_RFSOLENOID_TRANSFORM_HH
+#define UAL_RFSOLENOID_TRANSFORM_HH
 
-#ifndef UAL_SPINK_RFSOLENOID_HH
-#define UAL_SPINK_RFSOLENOID_HH
-
-#include "PAC/Beam/Position.hh"
 #include "SMF/PacLattElement.h"
-#include "SMF/PacElemRfCavity.h"
-#include "TEAPOT/Integrator/BasicTracker.hh"
 
-#include "SPINK/Propagator/SpinPropagator.hh" //AUL:27APR10
+#include "SMF/PacElemLength.h"
+#include "SMF/PacElemBend.h"
+#include "SMF/PacElemMultipole.h"
+#include "SMF/PacElemOffset.h"
+#include "SMF/PacElemRotation.h"
+#include "SMF/PacElemAperture.h"
+#include "SMF/PacElemComplexity.h"
+#include "SMF/PacElemSolenoid.h"
+#include "SMF/PacElemRfCavity.h"
+
+#include "SPINK/Propagator/SpinPropagator.hh"
 
 namespace SPINK {
 
-  /** Spink-specific RF Solenoid (Tracker) */
+  /** Basis class of different spin trackers */
 
-  class RFSolenoid : public TEAPOT::BasicTracker {
+  class RFSolenoid : public SpinPropagator {
 
   public:
 
     /** Constructor */
     RFSolenoid();
 
-    /** Copy Constructor */
-    RFSolenoid(const RFSolenoid& rft);
+   /** Copy constructor */
+    RFSolenoid(const RFSolenoid& st);
 
     /** Destructor */
-    virtual ~RFSolenoid();
+    ~RFSolenoid();
+    /** Defines the lattice elemements (PropagatorNode method)
+	Note: integers i0 and i1 will be replaced by AcceleratorNode's 
+    */
+    virtual void setLatticeElements(const UAL::AcceleratorNode& sequence, int i0, int i1, 
+				    const UAL::AttributeSet& attSet);
+
+    /** Propagates a bunch */
+    void propagate(UAL::Probe& bunch);
 
     /** Returns a deep copy of this object (inherited from UAL::PropagatorNode) */
     UAL::PropagatorNode* clone();
 
-    /** Set lattice elements (inherited from UAL::PropagatorNode */
-    void setLatticeElements(const UAL::AcceleratorNode& lattice, int i0, int i1, 
-			    const UAL::AttributeSet& beamAttributes);
+    static void setRFSParams(double b, char r, double f, double d, int n)
+    {RFS_Bdl = b; RFS_rot = r; RFS_freq0 = f; RFS_dfreq = d; RFS_nt = n;}
+ 
+    static double RFS_Bdl;
+    static char RFS_rot;
+    static double RFS_freq0;
+    static double RFS_dfreq;
+    static int RFS_nt;
 
-    //virtual void setLatticeElements(const UAL::AcceleratorNode& sequence, int i0, int i1, 
-    //				    const UAL::AttributeSet& attSet); //AUL:27APR10
-
-    /** Propagates a bunch of particles */
-    void propagate(UAL::Probe& probe);
-
-    /** Sets Rf parameters */
-    static void setRF(double V, double harmon, double lag) { 
-      m_V = V; m_h = harmon; m_lag = lag;
-    }
-
-    /** Pass ring length AUL:17MAR10 */
-    static void setCircum(double circum){circ = circum;}
-    static double circ ;
-
-     /** Setup a dump flag for diagnostics AUL:27APR10 */
+     /** Setup a dump flag for diagnostics AUL:01MAR10 */
     static void setOutputDump(bool outdmp){coutdmp = outdmp;}
     static bool coutdmp; 
 
-     /** Pass information on turn number for diagnostics AUL:27APR10 */
+     /** Pass information on turn number for diagnostics AUL:01MAR10 */
     static void setNturns(int iturn){nturn = iturn;}
     static int nturn ;
-  protected:
-
-    /** Sets the lattice element */
-    void setLatticeElement(const PacLattElement& e);
-
-    /** Propagates the particle through a drift */
-    void passDrift(double l, PAC::Position& p, double v0byc, double vbyc);
 
   protected:
 
-    /** Element length */
-    double m_l;
+    // Element data
 
-    /** Peak RF voltage [GeV] */ 
-    static double m_V;
+    void setElementData(const PacLattElement& e);
 
-    /** Phase lag in multiples of 2 pi */
-    static double m_lag;
+    std::string m_name;
 
-    /** Harmonic number */
-    static double m_h;
+    PacElemMultipole* p_entryMlt;
+    PacElemMultipole* p_exitMlt;
+
+    PacElemLength* p_length;           // 1: l
+    PacElemBend* p_bend;               // 2: angle, fint
+    PacElemMultipole* p_mlt;           // 3: kl, ktl
+    PacElemOffset* p_offset;           // 4: dx, dy, ds
+    PacElemRotation* p_rotation;       // 5: dphi, dtheta, tilt
+    // PacElemAperture*p_aperture;     // 6: shape, xsize, ysize
+    PacElemComplexity* p_complexity;   // 7: n
+    // PacElemSolenoid* p_solenoid;    // 8: ks
+    // PacElemRfCavity* p_rf;          // 9: volt, lag, harmon
+
+  protected:
+
+    // TEAPOT trackers
+
+    void setConventionalTracker(const UAL::AcceleratorNode& sequence,
+                                int is0, int is1,
+                                const UAL::AttributeSet& attSet);
+
+    /** Conventional tracker (body) */
+    UAL::PropagatorNodePtr m_tracker;
+
+    //  protected:
+
+  protected:
+
+    void propagateSpin(UAL::Probe& b);
+    void propagateSpin(PAC::BeamAttributes& ba, PAC::Particle& prt);
+
+    double get_psp0(PAC::Position& p, double v0byc);
+
 
   private:
 
-    void init();
-    void copy(const RFSolenoid& rft);
-    
+    void copy(const RFSolenoid& st);
+ 
   };
 
   class RFSolenoidRegister
   {
     public:
 
-      RFSolenoidRegister();
+    RFSolenoidRegister();
   };
+
 
 }
 
