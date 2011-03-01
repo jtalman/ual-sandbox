436ad258da561e669d6ccfc2b5051e4500e237fd
JohnDTalman <JohnDTalman@gmail.com>
2011-03-01 16:33:43 +0000

    Particle Data Group, Bill Morse

--- codes/UAL/src/UAL/Common/Def.hh
+++ codes/UAL/src/UAL/Common/Def.hh
@@ -11,7 +11,8 @@ namespace UAL {
   const double pi = 3.1415926536;
   
   /** Proton mass [GeV] */
-  const double pmass = 0.9382796;
+  const double pmass = 0.938272013;                      //  (23) GeV/c^2       0.9382796;
+  /** PDG, particle data group, July 2010 Particle Physics Booklet, IOP Publishing*/
 
   /** Electron mass [GeV] */
   const double emass = 0.5110340e-3;
@@ -26,8 +27,10 @@ namespace UAL {
   const double clight = 2.99792458e+8;
 
   /** elementary charge - http://physics.nist.gov/cgi-bin/cuu/Value?e|search_for=elecmag_in*/
-  /*  should verify with Handbook of Particle Physics*/
-  const double elemCharge = 1.602176487e-19;
+  /*  should verify with Handbook of Particle Physics?*/
+  /*  verified with PDG */
+  const double elemCharge = 1.602176487e-19;             // (40) C
+  /** PDG, particle data group, July 2010 Particle Physics Booklet, IOP Publishing*/
 
 }
 
