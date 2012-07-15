b5a01e8b73205c3535c7a2fcc5c46091efc2e727
JohnDTalman@gmail.com <JohnDTalman@gmail.com@9fb5c71f-954c-0410-a71a-f752f27b4993>
2012-07-15 15:40:39 +0000

    New interface that returns ring Twiss parameters (a0x,b0x,a0y,b0y) at the origin.

--- trunk/codes/ETEAPOT/src/Main/Eteapot.h
+++ trunk/codes/ETEAPOT/src/Main/Eteapot.h
@@ -19,7 +19,11 @@
 class Eteapot : public PacSmf
 {
 public:
-   void twissFromTracking( PAC::BeamAttributes ba, UAL::AcceleratorPropagator* ap, float m_m );
+   void twissFromTracking( PAC::BeamAttributes ba, UAL::AcceleratorPropagator* ap, float m_m,double& a0x,double& b0x,double& mu_xTent,double& a0y,double& b0y,double& mu_yTent );
+// void twissFromTracking( PAC::BeamAttributes ba, UAL::AcceleratorPropagator* ap, float m_m,double& a0x,double& b0x,double& a0y,double& b0y );
+// void twissFromTracking( PAC::BeamAttributes ba, UAL::AcceleratorPropagator* ap, float m_m,double a0x,double b0x,double a0y,double b0y );
+// void twissFromTracking( PAC::BeamAttributes ba, UAL::AcceleratorPropagator* ap, float m_m );
+
 // void twissFromTracking( PAC::BeamAttributes ba, UAL::AcceleratorPropagator* ap, std::string filename );
 // void twissFromTracking( PAC::BeamAttributes ba, UAL::AcceleratorPropagator* ap );
 };
