8ca00d98e196327305ee903a488c277be2dbda73
JohnDTalman@gmail.com <JohnDTalman@gmail.com@9fb5c71f-954c-0410-a71a-f752f27b4993>
2012-07-18 19:15:24 +0000

    extractParameters.h:     int splitForBends = 0;    // new split specification     int splitForQuads = 0;    // new split specification     int splitForSexts = 0;     // new split specification

--- trunk/examples/ETEAPOT/extractParameters.h
+++ trunk/examples/ETEAPOT/extractParameters.h
@@ -19,7 +19,10 @@
  orbitFile += mysxfbase;
  orbitFile += ".orbit";
 
- int split = 1;
+ int split = 1;            // old split specification
+ int splitForBends = 0;    // new split specification
+ int splitForQuads = 0;    // new split specification
+ int splitForSexts = 0;    // new split specification
  int order = 2;
  int turns;                // specified as 1 in trtrin (for post processing)
                            // might be overwritten tp multiple turns (e.g. 10) in simulatedProbeValues
