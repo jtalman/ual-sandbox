21ded5a9331d608f6d520858f74c6b29c598399c
JohnDTalman@gmail.com <JohnDTalman@gmail.com@9fb5c71f-954c-0410-a71a-f752f27b4993>
2013-12-29 23:09:34 +0000

    to facilitate cross checking between 2D_5Values and xyPlotLambda

--- trunk/examples/ETEAPOT/utilities/xyPlotLambda.cc
+++ trunk/examples/ETEAPOT/utilities/xyPlotLambda.cc
@@ -4,60 +4,29 @@
 #include<stdlib.h>
 #include<cstdlib>
 #include <iomanip>
-#include"../../../UAL/src/UAL/Common/Def.hh"
+#include"../clr"
+#include"../../../codes/UAL/src/UAL/Common/Def.hh"
 
 #define GAMMA_FROZEN_SPIN 1.248107349
 
 int main(int argc,char* argv[]){
- std::cout << setiosflags( std::ios::showpos   );
- std::cout << setiosflags( std::ios::uppercase );
- std::cout << setiosflags( std::ios::scientific );
-//std::cout << setw( 11 );
- std::cout << std::setfill( ' ' );
- std::cout << setiosflags( std::ios::left );
- std::cout << std::setprecision(13) ;
-
- std::cout << "JDT: file " << __FILE__ << " line " << __LINE__ << " argc " << argc << "\n";
- for(int i=0;i<argc;i++){
-  std::cout << i << " " << argv[i] << "\n";
- }
-
- if(argc!=6){
-  std::cout << "usage: ./xyPlotLambda typicalRadius LcOver_k  Efac  grid  oscillations\n";
-  std::cout << "                        (lambda)                               \n";
-  std::cout << "e.g. (single ellipse - first, leftmost, ellipse in Munozs Figure 3)\n";
-  std::cout << "       ./xyPlotLambda       2        3.23     0.99   100       1\n";
-  std::cout << "or  (reproduce Munozs Figure 3 - 16 ellipses)\n";
-  std::cout << "       ./xyPlotLambda 2 3.23 0.99 400 16\n";
-  exit(1);
- }
-
- double lambda  = atof(argv[1]);
- std::cout << "lambda  "    << lambda << "\n";
-
- double LcOver_k= atof(argv[2]);
- std::cout << "LcOver_k"    << LcOver_k<< "\n";
- double kapSqu  = 1-1/LcOver_k/LcOver_k;
- std::cout << "kapSqu  "    << kapSqu << "\n";
- double kap     = sqrt(kapSqu);
- std::cout << "kap     "    << kap    << "\n";
- double Efac    = atof(argv[3]);
- std::cout << "Efac    "    << Efac   << "\n";
- double eps     = LcOver_k*sqrt(1-kapSqu/Efac/Efac);
- std::cout << "eps     "    << eps    << "\n";
- std::cout <<                            "\n";
-
  std::ofstream fp("xyPlotLambda.out");
- double Ngrid   = atof(argv[4]);
- double delThta = 2*M_PI/Ngrid;
- int oscills    = atof(argv[5]);
- double     tau = oscills*2*M_PI;
- double    Thta = 0;
- double    r    = 0;
- double    x    = 0;
- double    y    = 0;
- for(Thta=0;Thta<tau;Thta+=delThta){
-  r = lambda/(1+eps*cos(kap*Thta));
+
+#include "inputs"
+
+ double kapSqu  = 1.-1./LcOver_k/LcOver_k;
+ double kap     = sqrt(kapSqu);
+ double eps     = LcOver_k*sqrt(1.-kapSqu/Efac/Efac);
+ double R0=lambda/(1.+eps);
+
+ double delThta = 2.*M_PI/Ngrid;
+ double     tau = oscills*2.*M_PI;
+ double    Thta = 0.;
+ double    r    = 0.;
+ double    x    = 0.;
+ double    y    = 0.;
+ for(Thta=0.;Thta<tau;Thta+=delThta){
+  r = lambda/(1.+eps*cos(kap*Thta));
   x = r * cos(Thta);
   y = r * sin(Thta);
          fp << x    << " " << y                             << "\n";
@@ -74,10 +43,26 @@ int main(int argc,char* argv[]){
  double mpcSqu  = mp*c*c;
  std::cout << "mp c^2  "    << mpcSqu << " (not strictly needed)\n";
 
- double kOverE  = lambda/(LcOver_k*LcOver_k-1);
+ double kOverE  = lambda/(LcOver_k*LcOver_k-1.);
  std::cout << "k/E     "    << kOverE << " (not strictly needed)\n";
  double k       = kOverE*Efac*mpcSqu;
  std::cout << "k       "    << k      << " (not strictly needed)\n";
+ double L=LcOver_k*k/c;
+ std::cout << "L       "    << L << "\n";
+ double E=k*1./kOverE;
+ std::cout << "E       "    << E << "\n";
+
+ double k2 = +2.68722e-09;
+//double c = +2.99792e+08;
+ double a = 1./LcOver_k;
+ double kapSqu2 = 1.-a*a;
+ std::cout << "k2      " << k2 << "\n";
+ std::cout << "kapSqu2 " << kapSqu2 << "\n";
+
+ double bSq=1./Efac/Efac-1.;
+ std::cout << "bSq     " << bSq << "\n";
+
+#include "outputs"
 
  return 0;
 }
