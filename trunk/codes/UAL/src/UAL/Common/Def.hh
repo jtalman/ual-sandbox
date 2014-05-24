8447736251e97f04c931b8530ec81cf906e70931
Richard.Talman@gmail.com <Richard.Talman@gmail.com@9fb5c71f-954c-0410-a71a-f752f27b4993>
2014-05-24 15:20:48 +0000

    Def.hh - Updated most parameters from NIST web site - RT

--- trunk/codes/UAL/src/UAL/Common/Def.hh
+++ trunk/codes/UAL/src/UAL/Common/Def.hh
@@ -6,19 +6,26 @@
  */
 
 namespace UAL {
+ /**  The source for these values is http://physics.nist.gov/cuu/Constants/index.html */
 
   /** Pi */
   const double pi = 3.1415926536;
   
   /** Proton mass [GeV] */
-  const double pmass = 0.938272013;                      //  (23) GeV/c^2       0.9382796;
+  const double pmass = 0.938272046;                      //  (23) GeV/c^2       0.9382796;
   /** PDG, particle data group, July 2010 Particle Physics Booklet, IOP Publishing*/
 
   /** Electron mass [GeV] */
-  const double emass = 0.5110340e-3;
+  const double emass = 0.510998928e-3;
 
-  /** Proton gyromagnetic ratio  */
-  const double pG = 1.7928456;
+  /** Proton g factor  */
+  const double pg = 5.585694713;
+
+  /** Proton anomalous magnetic moment  */
+  const double pG = 1.79284736;
+
+  /** Proton frozen spin gamma (direct function of pg) */
+  const double pFSG = 1.24810735;
 
   /** Infinity */
   const double infinity = 1.0e+20;
@@ -27,10 +34,8 @@ namespace UAL {
   const double clight = 2.99792458e+8;
 
   /** elementary charge - http://physics.nist.gov/cgi-bin/cuu/Value?e|search_for=elecmag_in*/
-  /*  should verify with Handbook of Particle Physics?*/
-  /*  verified with PDG */
   /* Proton/Electron charge [C] */
-  const double elemCharge = 1.602176487e-19;             // (40) C
+  const double elemCharge = 1.602176565e-19;             // (40) C
   /** PDG, particle data group, July 2010 Particle Physics Booklet, IOP Publishing*/
 
   /* Vacuum Permittivity [F/m] */
