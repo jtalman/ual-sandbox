3eb678ae9d351e33c6fcb76a88ef620fcdcda064
Richard.Talman <Richard.Talman@gmail.com>
2014-06-19 17:45:09 +0000

    getDesignBeam.h - update spin infrastructur - RT

---
+++ codes/ETEAPOT_MltTurn/src/ETEAPOT_MltTurn/Integrator/getDesignBeam.h
@@ -0,0 +1,11 @@
+double eD=cba.getEnergy();
+double mD=cba.getMass();
+double qD=cba.getCharge();
+double tD=cba.getElapsedTime();
+double fD=cba.getRevfreq();
+double MD=cba.getMacrosize();
+
+double gD=cba.get_g();
+double GD=cba.getG();
+
+double ElD=cba.getE();
